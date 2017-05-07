d3069b55a3adb44147003377e6c72fc2311b6e67
lacoski <123xuzu@gmail.com>
2017-05-07 00:04:00 -0400

    up dns and dhcp

---
+++ DNS/DNS/DNS.sh
@@ -0,0 +1,54 @@
+readonly base_file=`readlink -f "$0"` # Lay toan duong link file
+readonly base_path=`dirname $base_file` # get path link
+. "$base_path/lib/config.base"
+user_name=`whoami`
+if [ ! $user_name = "root" ]
+then
+	echo "You need root permission to run this script"
+	exit 1
+fi
+check_installed_yum()
+{
+	for i in ${packets[*]}
+	do
+		echo "Checking $i"
+		yum list installed | grep "$i"
+		if [ $? -ne "0" ]
+		then
+				echo "Installing $i"
+				yum install "$i" -y
+				echo "..Done"
+		else
+				echo "$i has installed!"
+				echo "$i updating packets"
+				yum install $i -y
+				echo "..Done"
+		fi
+	done
+}
+run_and_enable()
+{
+  systemctl enable named
+	systemctl start named
+}
+config_firewalld()
+{
+  firewall-cmd --permanent --zone=public --add-service=dns
+  firewall-cmd --reload
+	chgrp named -R /var/named
+	chown -v root:named /etc/named.conf
+	restorecon -rv /var/named
+	restorecon /etc/named.conf
+}
+config_main_path()
+{
+  cp -n $main_config $main_config.backup
+	cat $base_path/lib/main.config.temp > $main_config
+	\cp $base_path/lib/2.168.192.db $path_record
+	\cp $base_path/lib/home.lab.db	$path_record
+	systemctl restart named
+}
+check_installed_yum
+run_and_enable
+config_firewalld
+config_main_path
